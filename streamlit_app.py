b8d01618bbae9cbbed5f9897dfcfdea153dc9a73
nikumbhnikita <117893416+nikumbhnikita@users.noreply.github.com>
2023-04-18 10:38:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 streamlit.title("Hi Everyone")
 streamlit.title("Good morning")
+streamlit.text("Meeting Agenda :")
+streamlit.text("Meeting Agenda :")
+streamlit.text("1. Planning")
+streamlit.text("2. Execution")
+
 
